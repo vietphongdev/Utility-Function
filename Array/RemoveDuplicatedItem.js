13aaa65632cbb6b1aa0dc1e267116942466533b8
Viet Phong <vietphong.dev@gmail.com>
2023-07-01 11:16:20 +0700

    add removeDuplicatedItem

---
+++ Array/RemoveDuplicatedItem.js
@@ -0,0 +1,36 @@
+/**
+ * input = ["a", "b", "c", "d", "a", "c", "e"];
+ * output = ["b","d","e"]
+ */
+
+/** Dùng forEach */
+const removeDuplicatedItem = (input) => {
+  let output = [];
+  input.forEach((item) => {
+    if (!output.includes(item)) {
+      output = [...output, item];
+    } else {
+      output = output.filter((ele) => ele !== item);
+    }
+  });
+};
+
+/** Dùng Reduce */
+const removeDuplicatedItem = (arr) => {
+  const frequenceObj = arr.reduce((acc, item) => {
+    if(acc.hasOwnProperty(item)){
+      const {[item]: val, ...restAcc} = acc;
+      return restAcc;
+    }else{
+      acc[item] = 1;
+      return acc;
+    }
+  }, {});
+  return Object.keys(frequenceObj)
+};
+
+/** Test */
+const myArr = ["a", "b", "c", "d", "a", "c", "e"];
+const newArr = removeDuplicatedItem(myArr);
+console.log(newArr);
+console.log(myArr);
