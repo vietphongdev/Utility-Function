10f363c3dad47b515a0bc372c925daf4885d8706
Viet Phong <vietphong.dev@gmail.com>
2026-02-10 06:24:34 +0700

    sort array after merge

--- Array/SortArray.js
+++ Array/SortArray.js
@@ -10,15 +10,23 @@ Hãy tạo ra mảng C gồm:
 Output: [1, 2, 3, 5, 7, 8, 10, 15, 25, 28]
 */
 
+/** GIẢI THUẬT
+ * Clone một mảng C từ mảng A. 
+ * Tạo một biến tạm start
+ * Lặp qua từng phần tử của mảng B. (loop - i)
+ * Mỗi phần tử mảng B lặp qua từng phần tử mảng C để so sánh. (loop - j)
+ * Nếu nhỏ hơn thì xếp nó vào ngay bên trái của phần tử mảng C.
+ * Nếu bằng thì bỏ qua
+ * Nếu lớn hơn thì tiếp tục cho đến khi đến phần tử cuối mảng C, 
+ *  nếu vẫn lớn hơn thì push tất cả các phần tử còn lại của mảng B vào cuối mảng C và dừng chương trình.
+ */
+
 const A = [2, 7, 10, 15, 25];
-const B = [2, 6, 28, 30, 44];
+const B = [1, 6, 28, 30, 44];
 let C = [...A];
-let start = 0
+let start = 0;
 
-R1=[2,7,10,15,25];
-R2=[2,6,7,10,15,25];
-R3=[2,6,7,10,15,25];
-
+outerLoop:
 for(let i = 0; i < B.length; i++){
   for(let j = start; j < C.length; j++){
     if(B[i] === C[j]){
@@ -27,11 +35,20 @@ for(let i = 0; i < B.length; i++){
     };
     if(B[i] < C[j]){
       C.splice(j, 0, B[i]);
-      temp = j + 1;
+      start = j + 1;
       break;
     };
-    C.splice(j+1, 0, B[i]);
+    if(j === C.length - 1){
+      const restB = B.slice(i);
+      console.log(`restB >>`, restB)
+      C.concat(restB)
+      break outerLoop;
+    }
   };
-  console.log(C)
-}
+  console.log(`B-item: ${B[i]} -> C: ${C}`)
+};
+
+console.log('A >>', A)
+console.log('B >>', B)
+console.log('C >>', C)
 
